dadea9584b66df84f2170c16b519ae64d04b10ef
Husnain Zahid <husnainzahid214@gmail.com>
2023-08-12 17:56:26 +0500

    configured eslint for project and created assets export files

---
+++ src/assets/index.ts
@@ -0,0 +1,45 @@
+import footerPatternDesktop from './bg-pattern-footer-desktop.svg';
+import footerPatternMobile from './bg-pattern-footer-mobile.svg';
+import howWeWorkPatternDesktop from './bg-pattern-how-we-work-desktop.svg';
+import howWeWorkPatternMobile from './bg-pattern-how-we-work-mobile.svg';
+import introLeftPatternDesktop from './bg-pattern-intro-left-desktop.svg';
+import introLeftPatternMobile from './bg-pattern-intro-left-mobile.svg';
+import introRightPatternDesktop from './bg-pattern-intro-right-desktop.svg';
+import introRightPatternMobile from './bg-pattern-intro-right-mobile.svg';
+import navPattern from './bg-pattern-mobile-nav.svg';
+import logo from './logo.svg';
+import hamburger from './icon-hamburger.svg';
+import close from './icon-close.svg';
+import imageIntroDesktop from './image-intro-desktop.jpg';
+import imageIntroMobile from './image-intro-mobile.jpg';
+import snappyProcess from './icon-snappy-process.svg';
+import peopleFirst from './icon-people-first.svg';
+import affordablePrices from './icon-affordable-prices.svg';
+import facebook from './icon-facebook.svg';
+import twitter from './icon-twitter.svg';
+import pinterest from './icon-pinterest.svg';
+import instagram from './icon-instagram.svg';
+
+export {
+  footerPatternDesktop,
+  footerPatternMobile,
+  howWeWorkPatternDesktop,
+  howWeWorkPatternMobile,
+  introLeftPatternDesktop,
+  introLeftPatternMobile,
+  introRightPatternDesktop,
+  introRightPatternMobile,
+  navPattern,
+  logo,
+  hamburger,
+  close,
+  imageIntroDesktop,
+  imageIntroMobile,
+  snappyProcess,
+  peopleFirst,
+  affordablePrices,
+  facebook,
+  twitter,
+  pinterest,
+  instagram,
+};
